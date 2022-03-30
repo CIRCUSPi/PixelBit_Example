d34df1f86f8829ecb6f57402a1b5327bd105c50c
FelixLin <felix@atticedu.com>
2022-03-30 17:08:57 +0800

    add 04 FPV Car and 05 Dino Game Example

---
+++ 04_FPV_Car/PixelBit_FPV_Car_ESP32.ino/PixelBit_FPV_Car_ESP32.ino.ino
@@ -0,0 +1,377 @@
+/*********
+  Rui Santos
+  Complete instructions at https://RandomNerdTutorials.com/esp32-cam-projects-ebook/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h"             // disable brownout problems
+#include "soc/rtc_cntl_reg.h"    // disable brownout problems
+#include "esp_http_server.h"
+#include <tca5405.h>
+#include "config.h"
+
+TCA5405 tca5405;
+
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+#define CAMERA_MODEL_PIXEL_BIT // Has PSRAM
+
+#include "camera_pins.h"
+
+// Replace with your network credentials
+const char* ssid = "PixelBit";
+const char* password = "circuspi";
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t camera_httpd = NULL;
+httpd_handle_t stream_httpd = NULL;
+
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+<html>
+  <head>
+    <title>PixelBit FPV MoonCar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
+      table { margin-left: auto; margin-right: auto; }
+      td { padding: 8 px; }
+      .button {
+        background-color: #2f4468;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 18px;
+        margin: 6px 3px;
+        cursor: pointer;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+      }
+      img {  width: auto ;
+        max-width: 100% ;
+        height: auto ; 
+      }
+    </style>
+  </head>
+  <body>
+    <h1>PixelBit FPV MoonCar</h1>
+    <img src="" id="photo" >
+    <table>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('forward');" ontouchstart="toggleCheckbox('forward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Forward</button></td></tr>
+      <tr><td align="center"><button class="button" onmousedown="toggleCheckbox('left');" ontouchstart="toggleCheckbox('left');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Left</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('stop');" ontouchstart="toggleCheckbox('stop');">Stop</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('right');" ontouchstart="toggleCheckbox('right');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Right</button></td></tr>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('backward');" ontouchstart="toggleCheckbox('backward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Backward</button></td></tr>                   
+    </table>
+   <script>
+   function toggleCheckbox(x) {
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", "/action?go=" + x, true);
+     xhr.send();
+   }
+   window.onload = document.getElementById("photo").src = window.location.href.slice(0, -1) + ":81/stream";
+  </script>
+  </body>
+</html>
+)rawliteral";
+
+static esp_err_t index_handler(httpd_req_t *req) {
+  httpd_resp_set_type(req, "text/html");
+  return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
+}
+
+static esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
+
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if(fb->width > 400){
+        if(fb->format != PIXFORMAT_JPEG){
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if(!jpeg_converted){
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+    if(res == ESP_OK){
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if(fb){
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if(_jpg_buf){
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if(res != ESP_OK){
+      break;
+    }
+    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+  }
+  return res;
+}
+
+static esp_err_t cmd_handler(httpd_req_t *req){
+  char*  buf;
+  size_t buf_len;
+  char variable[32] = {0,};
+  
+  buf_len = httpd_req_get_url_query_len(req) + 1;
+  if (buf_len > 1) {
+    buf = (char*)malloc(buf_len);
+    if(!buf){
+      httpd_resp_send_500(req);
+      return ESP_FAIL;
+    }
+    if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+      if (httpd_query_key_value(buf, "go", variable, sizeof(variable)) == ESP_OK) {
+      } else {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+      }
+    } else {
+      free(buf);
+      httpd_resp_send_404(req);
+      return ESP_FAIL;
+    }
+    free(buf);
+  } else {
+    httpd_resp_send_404(req);
+    return ESP_FAIL;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  int res = 0;
+  
+  if(!strcmp(variable, "forward")) {
+    Serial.print(ESP32_CMD_FORWARD);
+  }
+  else if(!strcmp(variable, "left")) {
+    Serial.print(ESP32_CMD_LEFT);
+  }
+  else if(!strcmp(variable, "right")) {
+    Serial.print(ESP32_CMD_RIGHT);
+  }
+  else if(!strcmp(variable, "backward")) {
+    Serial.print(ESP32_CMD_BACK);
+  }
+  else if(!strcmp(variable, "stop")) {
+    Serial.print(ESP32_CMD_STOP);
+  }
+  else {
+    res = -1;
+  }
+
+  if(res){
+    return httpd_resp_send_500(req);
+  }
+
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+  return httpd_resp_send(req, NULL, 0);
+}
+
+void startCameraServer(){
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+  httpd_uri_t index_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = index_handler,
+    .user_ctx  = NULL
+  };
+
+  httpd_uri_t cmd_uri = {
+    .uri       = "/action",
+    .method    = HTTP_GET,
+    .handler   = cmd_handler,
+    .user_ctx  = NULL
+  };
+  httpd_uri_t stream_uri = {
+    .uri       = "/stream",
+    .method    = HTTP_GET,
+    .handler   = stream_handler,
+    .user_ctx  = NULL
+  };
+  if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(camera_httpd, &index_uri);
+    httpd_register_uri_handler(camera_httpd, &cmd_uri);
+  }
+  config.server_port += 1;
+  config.ctrl_port += 1;
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &stream_uri);
+  }
+}
+
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+void setup() {
+//  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
+#if defined(CAMERA_MODEL_PIXEL_BIT)
+  tca5405.init(21);
+  tca5405.set_gpo(PIXELBIT_CAMERA_POWER, 0);
+  tca5405.transmit();
+  delay(100);
+  tca5405.set_gpo(PIXELBIT_CAMERA_POWER, 1);
+  tca5405.transmit();
+  delay(100);
+#endif
+  
+  // initialize serial:
+  Serial.begin(UART_BAUDRATE);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(UART_BUFFER);
+  
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_hmirror(s, 1);
+  
+  // Wi-Fi connection
+#if WIFI_STA_MODE
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  Serial.print("Camera Stream Ready! Go to: http://");
+  Serial.println(WiFi.localIP());
+#else 
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+#endif
+  // Start streaming web server
+  startCameraServer();
+}
+
+
+void loop() {
+  if (stringComplete) {
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
---
+++ 05_Dino_Game/PixelBit_Dino_Game_esp32.ino/PixelBit_Dino_Game_esp32.ino.ino
@@ -0,0 +1,301 @@
+/*
+  CIRCUS Pi PixelBit Example
+  Dino Game - ESP32
+  A - start
+  B - jump
+*/
+#include <TFT_eSPI.h>
+#include "gameover.h"
+#include "noInternet.h"
+#include "imgData.h"
+#include "config.h"
+
+TFT_eSPI    tft = TFT_eSPI();
+TFT_eSprite img = TFT_eSprite(&tft);
+TFT_eSprite img2 = TFT_eSprite(&tft);
+TFT_eSprite e = TFT_eSprite(&tft);
+TFT_eSprite e2 = TFT_eSprite(&tft);
+
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+int dinoW = 33;
+int dinoH = 35;
+float linesX[6];
+int linesW[6];
+float linesX2[6];
+int linesW2[6];
+float clouds[2] = {(float)random(0, 80), (float)random(100, 180)};
+float bumps[2];
+int bumpsF[2];
+int eW = 18;
+int eH = 38;
+
+float eX[2] = {(float)random(240, 310), (float)random(380, 460)};
+int ef[2] = {0, 1};
+
+float roll_speed = GAME_SPEED;
+float cloudSpeed = 0.4;
+int x = 30;
+int y = 58;
+float dir = -1.4;
+int frames = 0;
+int f = 0;
+bool gameRun = 0;
+int score = 0;
+unsigned long start_t = 0;
+int t = 0;
+bool button_jump = 0;
+bool button_start = 0;
+
+void btn_b_pre(void) {
+#if DEBUG
+  Serial.println("bjump p");
+#endif
+  button_jump = true;
+}
+
+void btn_b_rel(void) {
+#if DEBUG
+  Serial.println("bjump r");
+#endif
+  // ignore this event, auto reset button_jump state
+  //  button_jump = false;
+}
+
+void btn_a_pre(void) {
+#if DEBUG
+  Serial.println("bstart p");
+#endif
+  button_start = true;
+}
+
+void btn_a_rel(void) {
+#if DEBUG
+  Serial.println("a r");
+#endif
+  button_start = false;
+
+}
+
+// Command Handler
+CMD_T cmd_list[] = {
+  {ATM_EVN_BTNA_PRE,      strlen(ATM_EVN_BTNA_PRE),     &btn_a_pre},
+  {ATM_EVN_BTNA_REL,      strlen(ATM_EVN_BTNA_REL),     &btn_a_rel},
+  {ATM_EVN_BTNB_PRE,      strlen(ATM_EVN_BTNB_PRE),     &btn_b_pre},
+  {ATM_EVN_BTNB_REL,      strlen(ATM_EVN_BTNB_REL),     &btn_b_rel},
+  {NULL, 0, NULL} //keep this elemnet in the end of list
+};
+
+void setup() {
+  // initialize serial:
+  Serial.begin(UART_BAUDRATE);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(UART_BUFFER);
+
+  tft.begin();
+  tft.setSwapBytes(true);
+  tft.fillScreen(TFT_WHITE);
+  tft.setRotation(3);
+  img.setTextColor(TFT_BLACK, TFT_WHITE);
+  img.setColorDepth(1);
+  img2.setColorDepth(1);
+  e.setColorDepth(1);
+  e2.setColorDepth(1);
+
+  img.createSprite(240, 100);
+  img2.createSprite(33, 35);
+  e.createSprite(eW, eH);
+  e2.createSprite(eW, eH);
+  tft.fillScreen(TFT_WHITE);
+
+
+  for (int i = 0; i < 6; i++) {
+    linesX[i] = random(i * 40, (i + 1) * 40);
+    linesW[i] = random(1, 14);
+    linesX2[i] = random(i * 40, (i + 1) * 40);
+    linesW2[i] = random(1, 14);
+  }
+
+  for (int n = 0; n < 2; n++) {
+    bumps[n] = random(n * 90, (n + 1) * 120);
+    bumpsF[n] = random(0, 2);
+  }
+  tft.pushImage(0, 0, 217, 135, noInternet);
+
+}
+
+
+void loop() {
+  static int  ii;
+
+  if (stringComplete) {
+    for (ii = 0; cmd_list[ii].len != 0; ii++) {
+      if (strcmp(cmd_list[ii].string, inputString.c_str()) == 0) {
+        //execute command
+        if (cmd_list[ii].handler != NULL) {
+          (*cmd_list[ii].handler)();
+        }
+        break;
+      }
+    }
+
+    if (cmd_list[ii].len == 0) {
+      //can't find command
+#if DEBUG
+      Serial.println("command not found.");
+      Serial.println(inputString);
+#endif
+    }
+
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+
+  if (gameRun == 1) {
+    if (button_jump == 1) {
+      f = 0;
+    }
+
+    if (button_jump == 1) {
+      y = y + dir * roll_speed;
+      if (y <= 2) {
+        y = 2; 
+        dir = dir * -1.00;
+      } else if (y >= 58) {
+        button_jump = 0;
+        dir = dir * -1.00;
+      }
+    }
+
+    if (frames < 8 && button_jump == 0) {
+      f = 1;
+    } if (frames > 8 && button_jump == 0) {
+      f = 2;
+    }
+
+    drawS(x, y, f);
+    frames++;
+    if (frames == 16)
+      frames = 0;
+
+    checkColision();
+  }
+
+  if (button_start == 1) {
+    button_start = 0;
+    if (gameRun == 0) {
+      gameRun = 1;
+      start_t = millis();
+      tft.fillScreen(TFT_WHITE);
+      eX[0] = random(240, 310);
+      eX[1] = random(380, 460);
+      button_jump = 0;
+      x = 30;
+      y = 58;
+      dir = -1.4;
+      roll_speed = GAME_SPEED;
+    }
+  }
+
+
+}
+
+void drawS(int x, int y, int frame) {
+
+  img.fillSprite(TFT_WHITE);
+  img.drawLine(0, 84, 240, 84, TFT_BLACK);
+
+  for (int i = 0; i < 6; i++) {
+    img.drawLine(linesX[i], 87 , linesX[i] + linesW[i], 87, TFT_BLACK);
+    linesX[i] = linesX[i] - roll_speed;
+    if (linesX[i] < -14) {
+      linesX[i] = random(245, 280);
+      linesW[i] = random(1, 14);
+    }
+    img.drawLine(linesX2[i], 98 , linesX2[i] + linesW2[i], 98, TFT_BLACK);
+    linesX2[i] = linesX2[i] - roll_speed;
+    if (linesX2[i] < -14) {
+      linesX2[i] = random(245, 280);
+      linesW2[i] = random(1, 14);
+    }
+  }
+
+  for (int j = 0; j < 2; j++) {
+    img.drawXBitmap(clouds[j], 20, cloud, 38, 11, TFT_BLACK, TFT_WHITE);
+    clouds[j] = clouds[j] - cloudSpeed;
+    if (clouds[j] < -40)
+      clouds[j] = random(244, 364);
+  }
+
+  for (int n = 0; n < 2; n++) {
+    img.drawXBitmap(bumps[n], 80, bump[bumpsF[n]], 34, 5, TFT_BLACK, TFT_WHITE);
+    bumps[n] = bumps[n] - roll_speed;
+    if (bumps[n] < -40) {
+      bumps[n] = random(244, 364);
+      bumpsF[n] = random(0, 2);
+    }
+  }
+
+  for (int m = 0; m < 2; m++) {
+
+    eX[m] = eX[m] - roll_speed;
+    if (eX[m] < -20)
+      eX[m] = random(240, 300);
+    ef[m] = random(0, 2);
+  }
+
+  e.drawXBitmap(0, 0, enemy[0], eW, eH, TFT_BLACK, TFT_WHITE);
+  e2.drawXBitmap(0, 0, enemy[1], eW, eH, TFT_BLACK, TFT_WHITE);
+  img2.drawXBitmap(0, 0, dino[frame], 33, 35, TFT_BLACK, TFT_WHITE);
+
+  e.pushToSprite(&img, eX[0], 56, TFT_WHITE);
+  e2.pushToSprite(&img, eX[1], 56, TFT_WHITE);
+  img2.pushToSprite(&img, x, y, TFT_WHITE);
+
+  score = (millis() - start_t) / 120;
+  img.drawString(String(score), 204, 0, 2);
+  //  img.drawString(String(roll_speed), 160, 0, 2);
+  img.pushSprite(0, 17);
+
+  if (score > t + GAME_SPEEDUP_SCORE) {
+    t = score;
+    roll_speed = roll_speed + GAME_SPEEDUP_GAP;
+  }
+
+}
+
+void checkColision()
+{
+
+  for (int i = 0; i < 2; i++) {
+    if (eX[i] < x + dinoW / 2 && eX[i] > x && y > 25) {
+      gameRun = 0;
+      tft.fillRect(0, 30, 240, 110, TFT_WHITE);
+      tft.drawXBitmap(10, 30, gameover, 223, 100, TFT_BLACK, TFT_WHITE);
+      delay(500);
+
+    }
+  }
+
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
