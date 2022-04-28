7acccfb9321cb8e1ba87b71d1d256ed9412debdb
Zack Huang <icshop.edu5@gmail.com>
2022-04-28 10:55:12 +0800

    save picture to sd card

--- 07_Pir_Camera_Line_Notify/PixelBit_Pir_Camera_ESP32/PixelBit_Pir_Camera_ESP32.ino
+++ 07_Pir_Camera_Line_Notify/PixelBit_Pir_Camera_ESP32/PixelBit_Pir_Camera_ESP32.ino
@@ -4,23 +4,25 @@
 #include "config.h"
 #include "esp_camera.h"
 #include "esp_system.h"
+#include <EEPROM.h>       // read and write from flash memory
 #include <TFT_eSPI.h>     // Hardware-specific library
 #include <TJpg_Decoder.h>
 #include <TridentTD_LineNotify.h>
 #include <WiFi.h>
 #include <tca5405.h>
 
-
 #define CAMERA_MODEL_PIXEL_BIT
 #include "camera_pins.h"
 
 #define PIR_PIN 3
+#define EEPROM_SIZE 1
 
 hw_timer_t *timer          = NULL;
 bool        pir_trig_state = false;
 TCA5405     tca5405;
 String      inputString    = "";        // a String to hold incoming data
 bool        stringComplete = false;     // whether the string is complete
+int         pictureNumber  = 0;
 
 void IRAM_ATTR resetModule()
 {
@@ -171,10 +173,33 @@ void Camera_capture()
     camera_fb_t *fb = NULL;
     fb              = esp_camera_fb_get();
     if (!fb) {
-        Serial.println("Camera capture failed");
+        // Serial.println("Camera capture failed");
         return;
     }
+
     LINE.notifyPicture("PixelBit_nono", fb->buf, fb->len);
+
+    // Serial.println("Starting SD Card");
+    uint8_t cardType = SD_MMC.cardType();
+    if (SD_MMC.begin() && cardType != CARD_NONE) {
+        // initialize EEPROM with predefined size
+        EEPROM.begin(EEPROM_SIZE);
+        pictureNumber = EEPROM.read(0) + 1;
+        // Path where new picture will be saved in SD Card
+        String  path = "/picture" + String(pictureNumber) + ".jpg";
+        fs::FS &fs   = SD_MMC;
+        // Serial.printf("Picture file name: %s\n", path.c_str());
+        File file = fs.open(path.c_str(), FILE_WRITE);
+        if (!file) {
+            // Serial.println("Failed to open file in writing mode");
+        } else {
+            file.write(fb->buf, fb->len);     // payload (image), payload length
+            // Serial.printf("Saved file to path: %s\n", path.c_str());
+            EEPROM.write(0, pictureNumber);
+            EEPROM.commit();
+        }
+        file.close();
+    }
     esp_camera_fb_return(fb);
 }
 
